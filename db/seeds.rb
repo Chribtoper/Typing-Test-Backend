5bccd52a0e5b4f4522e8f612a76eb4bb642c97a8
github username <github email address>
2018-12-04 19:31:42 -0500

    updated lyrics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,120 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# this file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# examples:
 #https://genius.com/Ariana-grande-thank-u-next-lyrics
 #
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Artist.create({name: 'Ariana Grande'})
+#   movies = movie.create([{ name: 'Star Wars' }, { name: 'lord of the rings' }])
+#   character.create(name: 'Luke', movie: movies.first)
+Artist.create({name: 'Ariana grande'})
 Artist.create({name: 'Travis Scott'})
 Artist.create({name: 'Drake'})
 Artist.create({name: 'The Beatles'})
 Artist.create({name: 'NSYNC'})
-Song.create({ title: 'Thank U, next', artist_id: '1', body: 'Thought Id end up with Sean/ But he wasnt a match/ Wrote some songs about Ricky/ Now I listen and laugh/ Even almost got married/ And for Pete, Im so thankful/ Wish I could say, "Thank you" to Malcolm/ Cause he was an angel/ One taught me love
-One taught me patience/ And one taught me pain/ Now, Im so amazing/ Say Ive loved and Ive lost/ But thats not what I see/ So, look what I got/ Look what you taught me/ And for that, I say', img_url: 'https://amp.thisisinsider.com/images/5c01c668ac00e24f0151bad4-750-563.jpg'})
-Song.create({title: 'Sicko Mode', artist_id: '2', body: 'Astro, yeah/Sun is down, freezin cold/ Thats how we already know, winters here/ My dawg would probably do it for a Louis belt/ Thats just all he know, he dont know nothing else/ I tried to show em, yeah I tried to show em, yeah, yeah/ Yeah, yeah, yeah/ Goin on you with the pick and roll/
-Young La Flame, he in sicko mode', img_url: 'https://i.ytimg.com/vi/d-JBBNg8YKs/maxresdefault.jpg'})
-Song.create({title: 'In My Feelings', artist_id: '3', body: "Trap, TrapMoneyBenny
-This shit got me in my feelings
-Gotta be real with it, yup
-Kiki, do you love me? Are you riding?
-Say you'll never ever leave from beside me
-'Cause I want ya, and I need ya
-And I'm down for you always
-KB, do you love me? Are you riding?
-Say you'll never ever leave from beside me
-'Cause I want ya, and I need ya
-And I'm down for you always
-Look, the new me is really still the real me
-I swear you gotta feel me before they try and kill me
-They gotta make some choices, they runnin' out of options
-'Cause I've been goin' off and they don't know when it's stoppin'
-And when you get to toppin', I see that you've been learnin'
-And when I take you shoppin' you spend it like you earned it
-And when you popped off on your ex he deserved it
-I thought you were the one from the jump, that confirmed it
-Trapmoneybenny
-I buy you champagne but you love some Henny
-From the block like you Jenny
-I know you special, girl, 'cause I know too many
-'Resha, do you love me? Are you riding?
-Say you'll never ever leave from beside me
-'Cause I want ya, and I need ya
-And I'm down for you always
-J.T., do you love me? Are you riding?
-Say you'll never ever leave from beside me
-'Cause I want ya, and I need ya
-And I'm down for you always
-Two bad bitches and we kissin' in the Wraith
-Kissin'-kissin' in the Wraith, kiss-kissin' in the Wraith
+Song.create({ title: 'Thank U, next', artist_id: '1', body: "thought I'd end up with sean but he wasn't a match wrote some songs about ricky now I listen and laugh even almost got married and for pete I'm so thankful wish I could say thank you to malcolm cause he was an angel one taught me love one taught me patience and one taught me pain now I'm so amazing say I've loved and I've lost but that's not what I see so look what I got look what you taught me and for that I say", img_url: 'https://amp.thisisinsider.com/images/5c01c668ac00e24f0151bad4-750-563.jpg'})
+Song.create({title: 'Sicko Mode', artist_id: '2', body: "astro yeah sun is down freezin cold that's how we already know winters here my dawg would probably do it for a louis belt that's just all he know he don't know nothing else I tried to show em yeah I tried to show em yeah yeah yeah yeah yeah goin on you with the pick and roll young la flame he in sicko mode", img_url: 'https://i.ytimg.com/vi/d-JBBNg8YKs/maxresdefault.jpg'})
+Song.create({title: 'In My feelings', artist_id: '3', body: "trap trap money benny this shit got me in my feelings gotta be real with it yup kiki do you love me are you riding say you'll never ever leave from beside me cause I want ya and I need ya and I'm down for you always kb do you love me are you riding say you'll never ever leave from beside me cause I want ya and I need ya and I'm down for you always look the new me is really still the real me I swear you gotta feel me before they try and kill me they gotta make some choices they runnin' out of options cause I've been going off and they don't know when it's stopping and when you get to topping I see that you've been learning and when I take you shopping you spend it like you earned it and when you popped off on your ex he deserved it I thought you were the one from the jump that confirmed it trap money benny I buy you champagne but you love some henny from the block like you jenny I know you special girl cause I know too many resha do you love me are you riding say you'll never ever leave from beside me cause I want ya and I need ya and I'm down for you always jt do you love me are you riding say you'll never ever leave from beside me cause I want ya and I need ya and I'm down for you always two bad bitches and we kissing in the wraith
+kissing-kissing in the wraith kiss-kissing in the wraith
 I need that black card and the code to the safe
-Code to the safe, code-code to the safe-safe
+code to the safe code-code to the safe-safe
 I show him how the neck work
-Fuck that Netflix and chill, what's your net-net-net worth?
-'Cause I want ya, and I need ya
-And I'm down for you always
-(Yeah, yeah)
-And I'm down for you always
+fuck that Netflix and chill what's your net-net-net worth
+'cause I want ya and I need ya
+and I'm down for you always
+(Yeah yeah)
+and I'm down for you always
 D-down for you always
 D-d-down for you always
-(I got a new boy, and that nigga trade)
-Kiki, do you love me? Are you riding?
-Say you'll never ever leave from beside me
-'Cause I want you, and I need you
-And I'm down for you always
-KB, do you love me? Are you riding?
-Say you'll never ever leave
-From beside me, 'cause I want ya, and I
-Bring that ass, bring that ass, bring that ass back
-B-bring that ass, bring that ass, bring that ass back
-Shawty say the nigga that she with can't hit
-But shawty, I'ma hit it, hit it like I can't miss
-(Clap that ass, you're the only one I love)
-(Clap that ass, clap, clap)
-Bring that ass back
-(Clap that ass, you're the only one I love)
-(Let's go, let's go)
-Bring that ass back
-Trap, TrapMoneyBenny
-This shit got me in my feelings
-I just gotta be real with it, yup", img_url: 'https://static1.squarespace.com/static/56858337cbced60d3b293aef/t/5b3d0cf8575d1fd7a4721500/1530727681375/Albumism_Drake_Scorpion_MainImage.jpg'
+(I got a new boy and that nigga trade)
+kiki do you love me are you riding
+say you'll never ever leave from beside me
+'cause I want you and I need you
+and I'm down for you always
+kb do you love me are you riding
+say you'll never ever leave
+from beside me 'cause I want ya and I
+bring that ass bring that ass bring that ass back
+b-bring that ass bring that ass bring that ass back
+shawty say the nigga that she with can't hit
+but shawty I'ma hit it hit it like I can't miss
+(clap that ass you're the only one I love)
+(clap that ass clap clap)
+bring that ass back
+(clap that ass you're the only one I love)
+(let's go let's go)
+bring that ass back
+trap trap money benny
+this shit got me in my feelings
+I just gotta be real with it yup", img_url: 'https://static1.squarespace.com/static/56858337cbced60d3b293aef/t/5b3d0cf8575d1fd7a4721500/1530727681375/Albumism_Drake_Scorpion_mainImage.jpg'
 })
-Song.create({title: 'Twist and Shout', artist_id: '4', body: "Well, shake it up baby now
-Twist and shout
-Come on, come on, come, come on baby now
-Come on and work it on out
-Well, work it on out, honey
-You know you look so good
-You know you got me goin' now
-Just like I know you would
-Well, shake it up baby now
-Twist and shout
-Come on, come on, come, come on baby now
-Come on and work it on out
-You know you twist, little girl
-You know you twist so fine
-Come on and twist a little closer now
-And let me know that you're mine, woo
-Ah, ah, ah, ah, wow
-Baby now
-Twist and shout
-Come on, come on, come, come on baby now
-Come on and work it on out
-You know you twist, little girl
-You know you twist so fine
-Come on and twist a little closer now
-And let me know that you're mine
-Well, shake it, shake it, shake it, baby now
-Well, shake it, shake it, shake it, baby now
-Well, shake it, shake it, shake it, baby now
-Ah, ah, ah, ah", img_url: 'https://media.pitchfork.com/photos/592f55a0e972aa3841725631/2:1/w_790/a5d7238c.jpg'})
+Song.create({title: 'Twist and Shout', artist_id: '4', body: "well shake it up baby now twist and shout come on come on come come on baby now come on and work it on out well work it on out honey you know you look so good you know you got me goin' now just like I know you would well shake it up baby now twist and shout come on come on come come on baby now come on and work it on out you know you twist little girl you know you twist so fine come on and twist a little closer now and let me know that you're mine woo ah ah ah ah wow baby now twist and shout come on come on come come on baby now come on and work it on out you know you twist little girl you know you twist so fine come on and twist a little closer now and let me know that you're mine well shake it shake it shake it baby now well shake it shake it shake it baby now well shake it shake it shake it baby now
+ah ah ah ah", img_url: 'https://media.pitchfork.com/photos/592f55a0e972aa3841725631/2:1/w_790/a5d7238c.jpg'})
 Song.create({title: 'Bye Bye Bye', artist_id: '5', body: "Bye bye bye
 I'm doing this tonight
 You're probably gonna start a fight
 I know this can't be right
-Hey baby come on
+hey baby come on
 I loved you endlessly
 When you weren't there for me
 So now it's time to leave and make it alone
@@ -123,7 +63,7 @@ It ain't no lie
 I want to see you out that door
 Baby bye bye bye
 Bye bye Don't want to be a fool for you
-Just another player in your game for two
+just another player in your game for two
 You may hate me but it ain't no lie
 Baby bye bye bye
 Bye bye
@@ -137,13 +77,13 @@ So give me one good reason
 Baby come on
 I've lived for you and me
 And now I really come to see
-That life would be much better once you're gone
+that life would be much better once you're gone
 I know that I can't take no more
 Ain't no lie
 I want to see you out that door
 Baby bye bye bye
 Bye bye Don't want to be a fool for you
-Just another player in your game for two
+just another player in your game for two
 You may hate me but it ain't no lie
 Baby bye bye bye
 Bye bye
@@ -160,12 +100,12 @@ Don't want to be your fool in this game for two
 So I'm leaving you behind
 (Bye bye bye)
 I don't want to make it tough
-(Make it tough)
+(make it tough)
 But I've had enough
 (Bye bye)
 And it ain't no lie
 Bye bye Don't want to be a fool for you
-Just another player in your game for two
+just another player in your game for two
 You may hate me but it ain't no lie
 Baby bye bye bye
 Bye bye
